438621bbdb36ad4fe2f287e9a4202e48191be881
[tekesashia] <kyaputenn13@gmail.com>
2022-02-26 22:29:07 +0900

    a

--- mypage/todolist/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import List
-
-admin.site.register(List)
-#adminでリストを追加できるようにしますよ
--- mypage/todolist/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-class List(models.Model):
-    list_item = models.CharField(max_length=200)
-
-    def __str__(self):
-        return self.list_item
